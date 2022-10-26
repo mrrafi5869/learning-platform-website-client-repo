9621334ac61cfb3302e66aab3b6d8ff0e2787b11
Mohammad Rafe <mrrafi5869@gmail.com>
2022-10-26 19:06:42 +0600

    finish pdf file

--- src/components/Course/CourseDetails.jsx
+++ src/components/Course/CourseDetails.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import jsPDF from 'jspdf'
 
 const CourseDetails = () => {
   const course = useLoaderData();
   const {img, name, sell, lesson} = course;
+
+  const pdfGenerate = () => {
+    let doc = new jsPDF('landscape', "px", "a4", 'false');
+    doc.addImage(img, 'PNG', 65, 20, 500, 400);
+    doc.addPage();
+    // doc.text(60, 60, `this is ${name} course.
+    //  Already sold ${sell}.
+    //  You have ${lesson} lesson.`);
+    doc.setFont('Helvertica', "bold");
+    doc.text(60, 60, "Name: ");
+    doc.text(60, 80, "sold: ");
+    doc.text(60, 100, "Lesson: ");
+    doc.setFont('Helvertica', "normal");
+    doc.text(100, 60, name);
+    doc.text(100, 80, sell);
+    doc.text(120, 100, lesson);
+    doc.save(`${name}.pdf`)
+  }
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl mx-auto my-60">
       <figure>
@@ -14,12 +34,13 @@ const CourseDetails = () => {
           {name}!
           <div className="badge badge-secondary">NEW</div>
         </h2>
-        <p>{sell}</p>
+        <p>Total sell: {sell}</p>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">{lesson}</div>
-          <button className="badge badge-outline">Download</button>
+          <button className="badge badge-outline hover:bg-gray-400" onClick={pdfGenerate}>Download</button>
           <Link to = '/course'className="badge badge-outline bg-gray-500 text-white">Back</Link>
         </div>
+        <button className="btn">Premium Course</button>
       </div>
     </div>
   );
